f7d2fed75429ba10af7f6a19a7f0593aa4d63f19
salmattia <mattia.salvetti3@gmail.com>
2021-12-02 11:49:30 +0100

    changed swift DGC dependency

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .package(
             name: "SwiftDGC",
             url: "https://github.com/eu-digital-green-certificates/dgca-app-core-ios",
-            from: "1.0.5")
+            .tag("1.0.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
