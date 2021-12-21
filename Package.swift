aa41a66eff9d53cefa0eaa3386cd2252eadad351
salmattia <mattia.salvetti3@gmail.com>
2021-12-21 15:46:59 +0100

    changed Package.swift

--- Package.swift
+++ Package.swift
@@ -30,6 +30,7 @@ let package = Package(
             url: "https://github.com/eu-digital-green-certificates/dgca-app-core-ios",
             .revision("78f9a7fb0e63c0e0bb9d9b0dfed359babcc25960")),
         .package(
+            name: "Realm",
             url: "https://github.com/realm/realm-cocoa.git", 
             from: "10.19.0")
     ],
