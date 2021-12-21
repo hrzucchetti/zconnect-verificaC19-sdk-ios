ce1d612e9e3f87567cf3053ef241a1fd6e6cd305
salmattia <mattia.salvetti3@gmail.com>
2021-12-21 14:48:39 +0100

    changed Package.swift

--- Package.swift
+++ Package.swift
@@ -29,10 +29,6 @@ let package = Package(
             name: "SwiftDGC",
             url: "https://github.com/eu-digital-green-certificates/dgca-app-core-ios",
             .revision("78f9a7fb0e63c0e0bb9d9b0dfed359babcc25960"))
-        .package(
-            name: "Realm",
-            url: "https://github.com/realm/realm-swift.git",
-            from: "10.19.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -43,7 +39,6 @@ let package = Package(
                 "SwiftyJSON",
                 "Alamofire",
                 "SwiftDGC",
-                "RealmSwift",
             ],
             resources: [.copy("Res")]
         )
