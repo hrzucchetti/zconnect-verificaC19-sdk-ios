82af629de3d4d96c3823942c9ecf944aa68fffab
salmattia <mattia.salvetti3@gmail.com>
2021-12-21 15:26:38 +0100

    changed Package.swift

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
                 "SwiftyJSON",
                 "Alamofire",
                 "SwiftDGC",
-                "Realm"
+                "realm-swift"
             ],
             resources: [.copy("Res")]
         )
