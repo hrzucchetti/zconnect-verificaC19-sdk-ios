b054c01f16fd64106f036db5d488766486d1e217
salmattia <mattia.salvetti3@gmail.com>
2023-01-27 16:00:58 +0100

    changed version to 1.1.14

--- Sources/ZConnectVerificaC19SDK/SDKConfig.swift
+++ Sources/ZConnectVerificaC19SDK/SDKConfig.swift
@@ -22,5 +22,5 @@ struct SDKConfig {
     static let baseUrl = "https://get.dgc.gov.it/v1/dgc/"
     static let certificateFilename = "get-dgc-gov-it.der"
     static let certificateEvaluator = "get.dgc.gov.it"
-    static let SDKVersion = "1.1.13"
+    static let SDKVersion = "1.1.14"
 }
